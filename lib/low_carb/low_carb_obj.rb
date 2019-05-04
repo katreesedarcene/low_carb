2b649f0fdd1f7541c0dda2bfdde2629824381dad
Kat B <katreesebarnes@gmail.com>
2019-05-04 13:43:58 +0000

    added data to lowcarbobj

--- lib/low_carb/low_carb_obj.rb
+++ lib/low_carb/low_carb_obj.rb
@@ -1,5 +1,30 @@
 class LowCarb::Obj 
-  
+    attr_accessor :name
+
+
+
+
+	    @@all = [] 
+
+
+	    def initialize(name)
+	        @name = name 
+	      
+	    end 
+
+
+	    def self.get_data 
+	        10.times do 
+	            @@all << self.new("Ribs")
+	        end 
+	    end 
+
+
+	    def self.all 
+	        @@all
+	    end 
+
+
   
   
   
