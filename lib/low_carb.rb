481f09509a6871ec44949baebbce71c263022a61
Kat B <katreesebarnes@gmail.com>
2019-05-06 13:40:25 +0000

    fixed loading issues

--- lib/low_carb.rb
+++ lib/low_carb.rb
@@ -2,5 +2,5 @@ require "nokogiri"
 require 'open-uri'
 
 require_relative "low_carb/version"
-require_relative "low_carb/fake_news_obj"
+require_relative "low_carb/low_carb_obj"
 require_relative "low_carb/cli"
