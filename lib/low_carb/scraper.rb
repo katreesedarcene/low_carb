6af242c5792d93b540cb79c7e53ab3f860901bcc
Kat B <katreesebarnes@gmail.com>
2019-05-11 22:49:31 +0000

    made changes to scraper

--- lib/low_carb/scraper.rb
+++ lib/low_carb/scraper.rb
@@ -7,12 +7,14 @@ class Scraper
   doc = Nokogiri::HTML(open(LOWCARB_URL))
   recipes = []
   
-  doc.css("div.site-container").each do |item|
-    item.css('p'). each do |recipe| 
-      
-    end
-      
+    doc.css("div.site-container").each do |item|
+    item.css('p').each do |link|
+     link.css('a').each do |recipe|
+        
+    recipe_link = "#{recipe.attr('href')}"
     
+     end
+    end
   end 
  end
 end
