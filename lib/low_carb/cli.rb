cd75d94dc675c439a3c3759b8d4c4a1dd2a88a6e
Kat B <katreesebarnes@gmail.com>
2019-05-12 01:29:55 +0000

    added to cli

--- lib/low_carb/cli.rb
+++ lib/low_carb/cli.rb
@@ -1,13 +1,42 @@
 class LowCarb::CLI
     
-    def start 
-        LowCarb::Obj.get_data 
-        puts LowCarb::Obj.all
-        Scraper.scrape_lowcarb
-    end 
-    def welcome
-     puts "Welcome To Best LowCarb Recipes" 
+   def self.call_user
+     user_input = nil 
+     while user_input != "Adios"
+      puts "Hola, Welcome to my 5 favorite easy Mexican Keto Recipes"
+      puts "To see these recipes type: 'recipes'"
+      puts "To exit type: 'Adios'"
       
+      user_input = gets.downcase.chomp
+      
+      when 'recipes'
+        self.display_recipes
+      when 'adios'
+        self.adios
+      end
     end
+  end
+  
+   def display_recipes
+     user_input = nil
+     counter = 1 
+     LowCarb::Obj.all.each do |recipe|
+       puts "#{counter}. #{recipe.name}"
+       counter += 1 
+     end
+     puts "Please enter the number of the recipe you want to try"
+   end
+    
+    
+    
+    # def start 
+    #     LowCarb::Obj.get_data 
+    #     puts LowCarb::Obj.all
+    #     Scraper.scrape_lowcarb
+    # end 
+    # def welcome
+    # puts "Welcome To Best LowCarb Recipes" 
+      
+    # end
     
 end
