c6b8ec5cde7b6b022260445d69e37ec0d6fb14d7
Kat B <katreesebarnes@gmail.com>
2019-05-12 00:54:51 +0000

    added url to scraper

--- lib/low_carb/scraper.rb
+++ lib/low_carb/scraper.rb
@@ -1,16 +1,22 @@
 
 class Scraper
-      LOWCARB_URL ="https://www.ibreatheimhungry.com/15-best-low-carb-mexican-recipes/"
-      PORKCHOP_URL =https://www.ibreatheimhungry.com/keto-pork-chops-al-pastor-low-carb/
+      PORKCHOP_URL ="https://www.ibreatheimhungry.com/keto-pork-chops-al-pastor-low-carb/"
+      JALAPENOPOP_URL = "https://www.ibreatheimhungry.com/bacon-wrapped-stuffed-jalapenos-2/"
+      MEATBALLS_URL = "https://www.ibreatheimhungry.com/green-chicken-enchilada-meatballs-low-carb-gluten-free/"
+      MACCHEESE_URL = "https://www.ibreatheimhungry.com/chorizo-green-chili-cauliflower-mac-cheese-low-carb/"
+      CARNEASADA_URL = "https://www.ibreatheimhungry.com/keto-carne-asada-chimichurri-whole-30/"
   def self.get_page(url)
       return Nokogiri::HTML(open(url))
   
   end
   def self.get_recipe(url)
-    doc = self.get_page(recipe_url)
+    doc = self.get_page(url)
     recipe_addr = doc.css(".tasty-jump-print-buttons .tasty-print-button")
     recipe_name = doc.css(".entry-header .entry-title").text
-    new_recipe = LowCarb::Obj
+    new_recipe = LowCarb::Obj.new
+    new_recipe.name = recipe_name
+    new_recipe.web_addr = recipe_addr
+    new_recipe.save 
   end
   
 end
